3d21fb9d6646a20783814547e809183ed0a3cf4b
nasu-1103 <ayachooo113@gmail.com>
2024-09-06 20:40:14 +0900

    マイグレーションファイルの追加

---
+++ database/migrations/2024_06_02_092914_create_animes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('anime_group_id')->nullable();
+            $table->string('title');
+            $table->integer('episode');
+            $table->string('sub_title')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animes');
+    }
+};
