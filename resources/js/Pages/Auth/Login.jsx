c5c47707b2dae0d373142115d546f103dc6bedc9
nasu-1103 <ayachooo113@gmail.com>
2024-09-07 21:39:01 +0900

    リンクの追加

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -73,6 +73,13 @@ export default function Login({ status, canResetPassword }) {
                 </div>
 
                 <div className="flex items-center justify-end mt-4">
+                    <Link
+                        href={route('register')}
+                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        会員登録
+                    </Link>
+                    
                     {canResetPassword && (
                         <Link
                             href={route('password.request')}
