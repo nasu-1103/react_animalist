068027e5a15bca7ace148ae11012a973b887bdb5
nasu-1103 <ayachooo113@gmail.com>
2024-09-07 19:58:11 +0900

    ログインツールの実装

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('admin_flg')->nullable()->default(0);
             $table->timestamps();
         });
 
