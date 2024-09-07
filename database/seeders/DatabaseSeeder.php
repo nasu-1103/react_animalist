a793077536f975848178eec84fd33211b8e6af56
nasu-1103 <ayachooo113@gmail.com>
2024-09-07 19:54:01 +0900

    シーダーの編集

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Anime;
+use App\Models\AnimeGroup;
+use App\Models\User;
+use App\Models\WatchList;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,9 +19,26 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'admin_flg' => 1,
+        ]);
+
+        User::factory()->create([
+            'name' => 'なす',
+            'email' => 'test@test.com',
+        ]);
+
+        $this->call([
+            // AnimeSeeder::class,
+            // AnimeGroupSeeder::class,
+            // WatchListSeeder::class,
         ]);
     }
 }
