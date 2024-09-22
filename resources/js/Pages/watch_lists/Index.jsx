84200270c981c552ed0527a2e823011de6b11480
nasu-1103 <ayachooo113@gmail.com>
2024-09-22 10:56:05 +0900

    アニメグループリストの設定

--- resources/js/Pages/watch_lists/Index.jsx
+++ resources/js/Pages/watch_lists/Index.jsx
@@ -15,6 +15,9 @@ export default function WatchList({ auth, animeGroups, flash_message = null, err
     const [flashMessage, setFlashMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('')
 
+    // アニメグループリストの設定
+    const animeGroupList = [];
+
     // キーワードと一致するアニメグループを検索
     const animeGroupsLocal = animeGroups.filter(
         animeGroup => animeGroup.name.includes(data.keyword) ||
@@ -50,7 +53,7 @@ export default function WatchList({ auth, animeGroups, flash_message = null, err
                         <tr key={anime.id} className="text-center">
                             <td className="border border-slate-300 px-6 py-4">{anime.episode}話</td>
                             <td className="border border-slate-300 px-6 py-4">{anime.sub_title}</td>
-                            <td className="border border-slate-300 px-6 py-4">{anime.watchlists.created_at}</td>
+                            <td className="border border-slate-300 px-6 py-4">{anime.watchlists?.created_at}</td>
                             <td className="border border-slate-300 px-6 py-4">
                                 <select onChange={changeStatus} data-id={anime.id}>
                                     <option value="-1" selected={!anime.watchlists || anime.watchlists.status === -1}>未視聴</option>
@@ -149,13 +152,11 @@ export default function WatchList({ auth, animeGroups, flash_message = null, err
                                                                         <tr key={watchList.id} className="text-center">
                                                                             <td className="border border-slate-300 px-6 py-4">{anime.episode}話</td>
                                                                             <td className="border border-slate-300 px-6 py-4">{anime.sub_title}</td>
-                                                                            <td className="border border-slate-300 px-6 py-4">{watchList.created_at}</td>
-                                                                            <td className="border border-slate-300 px-6 py-4">
+                                                                            <td className="border border-slate-300 px-6 py-4">{watchList?.created_at}</td>
+                                                                            <td className="border border-slate-300 px-6 py-4 align-top rounded-xl">
                                                                                 {watchList.status === 1 ? '✅' : '👀'}
                                                                             </td>
-                                                                            <td className="flex border border-slate-300 px-6 py-4 justify-center gap-4">
-                                                                                <Link href="#" className="btn btn-outline btn-primary">編集</Link>
-                                                                                <button className="btn btn-outline btn-secondary" onClick={() => confirm('本当に削除しますか？')}>
+                                                                            <td className="flex border border-slate-300 px-6 py-4 justify-center gap-4">                                                                                <button className="btn btn-outline btn-secondary" onClick={() => confirm('本当に削除しますか？')}>
                                                                                     <svg
                                                                                         xmlns="http://www.w3.org/2000/svg"
                                                                                         fill="none"
@@ -170,7 +171,7 @@ export default function WatchList({ auth, animeGroups, flash_message = null, err
                                                                                             d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
                                                                                         />
                                                                                     </svg>
-                                                                                </button>
+                                                                        </button>
                                                                             </td>
                                                                         </tr>
                                                                     ))
