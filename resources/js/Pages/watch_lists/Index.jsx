4337eebf8d9f0d767f0038950244e8a33ebfa982
nasu-1103 <ayachooo113@gmail.com>
2024-09-22 21:39:30 +0900

    画面の編集

--- resources/js/Pages/watch_lists/Index.jsx
+++ resources/js/Pages/watch_lists/Index.jsx
@@ -53,16 +53,27 @@ export default function WatchList({ auth, animeGroups, flash_message = null, err
                         <tr key={anime.id} className="text-center">
                             <td className="border border-slate-300 px-6 py-4">{anime.episode}話</td>
                             <td className="border border-slate-300 px-6 py-4">{anime.sub_title}</td>
-                            <td className="border border-slate-300 px-6 py-4">{anime.watchlists?.created_at}</td>
                             <td className="border border-slate-300 px-6 py-4">
-                                <select onChange={changeStatus} data-id={anime.id}>
+                                {/* 日付を「YYYY-MM-DD」形式で取得 */}
+                                {
+                                    new Date(anime.watchlists?.created_at).getFullYear() + '-' +
+                                    ('0' + (new Date(anime.watchlists?.created_at).getMonth() + 1)).slice(-2) + '-' +
+                                    ('0' + (new Date(anime.watchLists?.created_at).getDate())).slice(-2)
+                                }
+                            </td>
+                            <td className="border border-slate-300 px-6 py-4">
+                                <select onChange={changeStatus} data-id={anime.id} className='rounded-xl'>
                                     <option value="-1" selected={!anime.watchlists || anime.watchlists.status === -1}>未視聴</option>
                                     <option value="2" selected={anime.watchlists?.status == 2}>視聴中</option>
                                     <option value="1" selected={anime.watchlists?.status == 1}>視聴済み</option>
                                 </select>
                             </td>
                             <td className="flex border border-slate-300 px-6 py-6 justify-center gap-4">
-                                <button className="btn btn-outline btn-secondary" onClick={deleteWatchList} data-id={anime.watchlists?.id}>削除</button>
+                                <button className="btn btn-outline btn-secondary" onClick={deleteWatchList} data-id={anime.watchlists?.id}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    </svg>
+                                </button>
                             </td>
                         </tr>
                     )}
@@ -119,6 +130,7 @@ export default function WatchList({ auth, animeGroups, flash_message = null, err
                             {errorMessage && <div className="mb-4 text-gray-700">{errorMessage}</div>}
                         </div>
 
+                        {/* 一致したアニメグループのリストを表示 */}
                         {animeGroupsLists}
 
                         {animeGroupList.length > 0 ? (
@@ -157,21 +169,21 @@ export default function WatchList({ auth, animeGroups, flash_message = null, err
                                                                                 {watchList.status === 1 ? '✅' : '👀'}
                                                                             </td>
                                                                             <td className="flex border border-slate-300 px-6 py-4 justify-center gap-4">                                                                                <button className="btn btn-outline btn-secondary" onClick={() => confirm('本当に削除しますか？')}>
-                                                                                    <svg
-                                                                                        xmlns="http://www.w3.org/2000/svg"
-                                                                                        fill="none"
-                                                                                        viewBox="0 0 24 24"
-                                                                                        stroke-width="1.5"
-                                                                                        stroke="currentColor"
-                                                                                        class="size-6"
-                                                                                    >
-                                                                                        <path
-                                                                                            stroke-linecap="round"
-                                                                                            stroke-linejoin="round"
-                                                                                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                                                                                        />
-                                                                                    </svg>
-                                                                        </button>
+                                                                                <svg
+                                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                                    fill="none"
+                                                                                    viewBox="0 0 24 24"
+                                                                                    stroke-width="1.5"
+                                                                                    stroke="currentColor"
+                                                                                    class="size-6"
+                                                                                >
+                                                                                    <path
+                                                                                        stroke-linecap="round"
+                                                                                        stroke-linejoin="round"
+                                                                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                                                                    />
+                                                                                </svg>
+                                                                            </button>
                                                                             </td>
                                                                         </tr>
                                                                     ))
