0a640bd1c1a06e87b3edad0c97577707973aae33
nasu-1103 <ayachooo113@gmail.com>
2024-09-25 21:53:07 +0900

    画面レイアウトの編集

--- resources/js/Pages/watch_lists/Index.jsx
+++ resources/js/Pages/watch_lists/Index.jsx
@@ -3,8 +3,8 @@ import { Head, Link, useForm } from '@inertiajs/react';
 import { useState, useEffect } from 'react';
 
 export default function WatchList({ auth, animeGroups, flash_message = null, error_message = null }) {
-    // 検索キーワードを入力するための初期データを設定
     const { data, setData, post, delete: destroy } = useForm({
+        // 検索キーワードの初期値を設定
         'keyword': ''
     });
 
@@ -28,66 +28,71 @@ export default function WatchList({ auth, animeGroups, flash_message = null, err
     const animeGroupsLists = animeGroupsLocal.map(animeGroup =>
         <>
             {/* アニメグループの情報を表示 */}
-            <div class="flex">
-                <p className='text-2xl'>{animeGroup.name}</p>
-                {/* 全てのアニメが視聴済みなら、👑を表示 */}
-                {animeGroup.animes_count == animeGroup.watchList_count &&
-                    <p className="text-2xl ml-2">👑</p>
-                }
-            </div>
-
-            <table className="w-full text-gray-700 text-nowrap">
-                <thead>
-                    <tr>
-                        <th className="mt-4 w-24">話数</th>
-                        <th className="mt-4 w-72">サブタイトル</th>
-                        <th className="mt-4 w-48">視聴日</th>
-                        <th className="mt-4 w-16">ステータス</th>
-                        <th className="mt-4 w-36">エディット</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {animeGroup.animes.map(anime => {
-                        const createdAt = anime.watchlists?.created_at;
-
-                        // Dateオブジェクトに変換
-                        const dateObj = createdAt ? new Date(createdAt) : null;
-
-                        // 年月日時分秒をそれぞれ取得
-                        const year = dateObj ? dateObj.getFullYear() : "----";
-                        const month = dateObj ? String(dateObj.getMonth() + 1).padStart(2, '0') : "--";
-                        const day = dateObj ? String(dateObj.getDate()).padStart(2, '0') : "--";
-                        const hours = dateObj ? String(dateObj.getHours()).padStart(2, '0') : "--";
-                        const minutes = dateObj ? String(dateObj.getMinutes()).padStart(2, '0') : "--";
-                        const seconds = dateObj ? String(dateObj.getSeconds()).padStart(2, '0') : "--";
-
-                        // フォーマットされた日時
-                        const formattedDateTime = dateObj ? `${year}-${month}-${day} ${hours}:${minutes}:${seconds}` : "";
-
-                        return (
-                            <tr key={anime.id} className="text-center">
-                                <td className="border border-slate-300 px-6 py-4">{anime.episode}話</td>
-                                <td className="border border-slate-300 px-6 py-4">{anime.sub_title}</td>
-                                <td className="border border-slate-300 px-6 py-4">{formattedDateTime}</td>
-                                <td className="border border-slate-300 px-6 py-4">
-                                    <select onChange={changeStatus} data-id={anime.id} className='rounded-xl'>
-                                        <option value="-1" selected={!anime.watchlists || anime.watchlists.status === -1}>未視聴</option>
-                                        <option value="2" selected={anime.watchlists?.status == 2}>視聴中</option>
-                                        <option value="1" selected={anime.watchlists?.status == 1}>視聴済み</option>
-                                    </select>
-                                </td>
-                                <td className="flex border border-slate-300 px-6 py-6 justify-center gap-4">
-                                    <button className="btn btn-outline btn-secondary" onClick={deleteWatchList} data-id={anime.watchlists?.id}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                        </svg>
-                                    </button>
-                                </td>
+            <div className="card bg-base-100 shadow-xl mt-6 text-lg">
+                <div className="card-body flex">
+                    <div class="flex">
+                        <h2 className='card-title'>{animeGroup.name}</h2>
+                        {/* 全てのアニメが視聴済みなら、👑を表示 */}
+                        {animeGroup.animes_count == animeGroup.watchList_count &&
+                            <span className="text-2xl ml-2">👑</span>
+                        }
+                    </div>
+                    <table className="w-full text-gray-700 text-nowrap">
+                        <thead>
+                            <tr>
+                                <th className="mt-4 w-24">話数</th>
+                                <th className="mt-4 w-72">サブタイトル</th>
+                                <th className="mt-4 w-48">視聴日</th>
+                                <th className="mt-4 w-16">ステータス</th>
+                                <th className="mt-4 w-36">エディット</th>
                             </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+                        </thead>
+                        <tbody>
+                            {/* アニメごとにテーブルを作成 */}
+                            {animeGroup.animes.map(anime => {
+                                // アニメの視聴日を取得
+                                const createdAt = anime.watchlists?.created_at;
+
+                                // Dateオブジェクトに変換
+                                const dateObj = createdAt ? new Date(createdAt) : null;
+
+                                // 年月日時分秒をそれぞれ取得
+                                const year = dateObj ? dateObj.getFullYear() : "----";
+                                const month = dateObj ? String(dateObj.getMonth() + 1).padStart(2, '0') : "--";
+                                const day = dateObj ? String(dateObj.getDate()).padStart(2, '0') : "--";
+                                const hours = dateObj ? String(dateObj.getHours()).padStart(2, '0') : "--";
+                                const minutes = dateObj ? String(dateObj.getMinutes()).padStart(2, '0') : "--";
+                                const seconds = dateObj ? String(dateObj.getSeconds()).padStart(2, '0') : "--";
+
+                                // フォーマットされた日時
+                                const formattedDateTime = dateObj ? `${year}-${month}-${day} ${hours}:${minutes}:${seconds}` : "";
+
+                                return (
+                                    <tr key={anime.id} className="text-center">
+                                        <td className="border border-slate-300 px-6 py-4">{anime.episode}話</td>
+                                        <td className="border border-slate-300 px-6 py-4">{anime.sub_title}</td>
+                                        <td className="border border-slate-300 px-6 py-4">{formattedDateTime}</td>
+                                        <td className="border border-slate-300 px-6 py-4">
+                                            <select onChange={changeStatus} data-id={anime.id} className='rounded-xl'>
+                                                <option value="-1" selected={!anime.watchlists || anime.watchlists.status === -1}>未視聴</option>
+                                                <option value="2" selected={anime.watchlists?.status == 2}>視聴中</option>
+                                                <option value="1" selected={anime.watchlists?.status == 1}>視聴済み</option>
+                                            </select>
+                                        </td>
+                                        <td className="flex border border-slate-300 px-6 py-6 justify-center gap-4">
+                                            <button className="btn btn-outline btn-secondary" onClick={deleteWatchList} data-id={anime.watchlists?.id}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                </svg>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </>
     );
 
@@ -143,72 +148,6 @@ export default function WatchList({ auth, animeGroups, flash_message = null, err
                         {animeGroupList.length > 0 ? (
                             animeGroupList.map((animeGroup) => (
                                 <div key={animeGroup.id} className="text-gray-900">
-                                    <div className="card bg-base-100 shadow-xl mt-6 text-lg">
-                                        <div className="card-body flex">
-                                            <div className="flex">
-                                                <h2 className="card-title">{animeGroup.name}</h2>
-                                                {/* 全アニメがウォッチリストに登録されている場合は👑を表示 */}
-                                                {animeGroup.animes.length === animeGroup.animes.reduce(
-                                                    (init, anime) => init + anime.watchlists.length, 0) && (
-                                                        <span className="text-2xl ml-2">👑</span>
-                                                    )}
-                                                {/* アニメごとにテーブルを作成 */}
-                                                {animeGroup.animes.map((anime) => (
-                                                    <div key={anime.id} className="anime_group">
-                                                        <table className="w-full text-gray-700 text-nowrap">
-                                                            <thead>
-                                                                <tr>
-                                                                    <th className="mt-4 w-24">話数</th>
-                                                                    <th className="mt-4 w-72">サブタイトル</th>
-                                                                    <th className="mt-4 w-48">視聴日</th>
-                                                                    <th className="mt-4 w-16">ステータス</th>
-                                                                    <th className="mt-4 w-36">エディット</th>
-                                                                </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                                {anime.watchlists.length > 0 ? (
-                                                                    anime.watchlists.map((watchList) => (
-                                                                        <tr key={watchList.id} className="text-center">
-                                                                            <td className="border border-slate-300 px-6 py-4">{anime.episode}話</td>
-                                                                            <td className="border border-slate-300 px-6 py-4">{anime.sub_title}</td>
-                                                                            <td className="border border-slate-300 px-6 py-4">{watchList?.created_at}</td>
-                                                                            <td className="border border-slate-300 px-6 py-4 align-top rounded-xl">
-                                                                                {watchList.status === 1 ? '✅' : '👀'}
-                                                                            </td>
-                                                                            <td className="flex border border-slate-300 px-6 py-4 justify-center gap-4">                                                                                <button className="btn btn-outline btn-secondary" onClick={() => confirm('本当に削除しますか？')}>
-                                                                                <svg
-                                                                                    xmlns="http://www.w3.org/2000/svg"
-                                                                                    fill="none"
-                                                                                    viewBox="0 0 24 24"
-                                                                                    stroke-width="1.5"
-                                                                                    stroke="currentColor"
-                                                                                    class="size-6"
-                                                                                >
-                                                                                    <path
-                                                                                        stroke-linecap="round"
-                                                                                        stroke-linejoin="round"
-                                                                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                                                                                    />
-                                                                                </svg>
-                                                                            </button>
-                                                                            </td>
-                                                                        </tr>
-                                                                    ))
-                                                                ) : (
-                                                                    // ウォッチリストにデータがなくても罫線を引く
-                                                                    <tr>
-                                                                        <td className="border border-slate-300 px-6 py-4"></td>
-                                                                        <td className="border border-slate-300 px-6 py-4"></td>
-                                                                        <td className="border border-slate-300 px-6 py-4"></td>
-                                                                    </tr>
-                                                                )}
-                                                            </tbody>
-                                                        </table>
-                                                    </div>
-                                                ))}
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                             ))
                         ) : (
